7cd43cc0afd6a05676a3ca912f130f86a51cb8e0
thearnavgarg <thearnavgarg@gmail.com>
2018-11-17 21:54:49 -0800

    Can create the docker file now. Given a sample config file, the code can now create a docker file. It needs a dependencies json file to let it know what depends on what when creating a Docker file. This is being built on-top of an ubuntu image.

--- run.py
+++ run.py
@@ -1,6 +1,49 @@
 import argparse
 import sys
 import json
+import pdb
+
+def create_image_name(config):
+    name = [framework for framework in config['processing']['framework']]
+    name.append('base')
+    # TODO: For now the name is just the framework name, later we can add more. 
+    return ''.join(name)
+
+
+def create_docker_file(config, dep): 
+    def create_docker_file_helper(framework, dep, files):
+        if 'dep' not in dep[framework]:
+            files.append(dep[framework]['install'])
+            return
+        for depend in dep[framework]['dep']:
+            create_docker_file_helper(depend, dep, files)
+        files.append(dep[framework]['install'])
+
+    base_dockerfile = dep['base']['install']
+    files = [base_dockerfile]
+    for framework in config['processing']['framework']:
+        create_docker_file_helper(framework, dep, files) 
+
+    with open('config/Dockerfile', 'w') as outfile:
+        for fname in files:
+            with open(fname) as f:
+                for line in f:
+                    outfile.write(line)
+
+
+def create_image(config):
+    dep = json.load(open('dependencies.json'))
+    create_docker_file(config, dep)
+
+
+def image_exists(image_name):
+    #TODO: find a way to check if the image exists
+    return False
+
+
+def deploy(config):
+    #TODO: Run the image:
+    return
 
 
 if __name__ == '__main__':
@@ -20,6 +63,9 @@ if __name__ == '__main__':
     except FileNotFoundError:
         print('Error opening the config file')
         sys.exit(0)
-    print(config)
     
+    print(create_image_name(config))
+    create_image(config)
+#    if not image_exists(config):
+#        create_image(config)
 
