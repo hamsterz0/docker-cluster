20febe27f67e103b110ecc759c0945dd6895700d
thearnavgarg <thearnavgarg@gmail.com>
2018-12-01 14:46:45 -0800

    working code committed

--- docker_cluster.py
+++ docker_cluster.py
@@ -27,12 +27,20 @@ class DockerCluster:
         self.image = Image(self.info['image'])
     
     def deploy(self):
+        """
+            Deploy the network.
+        """
         # TODO: @Ken, deloy code here. 
         pass
 
     def run(self):
+        """
+            Initial run method.
+        """
         if not self.image.exists():
             self.image.create()
+        else:
+            print('Image exists -- Test')
         self.deploy()
 
 
@@ -45,12 +53,15 @@ class Image:
         self.framework = Framework(self.info['framework'])
 
     def create(self):
+        """
+            Method to create the image. 
+        """
         # Checking for the framework support. 
         if not self.framework.support():
             print('''Unfortunately, we don\'t support one or more of the frameworks 
                      you have listed in your config file. Please read the docs for
                      more information. Thank you.''')
-            sys.exit(0)     # Better to throw an exception instead but meeeh. 
+            sys.exit(0)     # Better to throw an exception instead but meeeh.
         # Working on creating an image. 
         if os.path.isdir(self.image_name):
             os.system('rm -rf '+self.image_name)
@@ -61,15 +72,23 @@ class Image:
         with open(docker_file_path, 'w') as df:
             for line in docker_file_contents:
                 df.write(line)
-        os.system('docker build ./' + self.image_name + ' -t ' + self.image_name)
+        os.system('docker build ./' + self.image_name + ' -t ' + self.image_name)   # Build the image.
 
     def exists(self):
+        """
+            Method to check if the image exists. 
+
+            Returns the image name.
+        """
         image_list = os.popen('docker image ls').read()
         print(self.image_name)
         return self.image_name in [names.split(' ')[0] for names in image_list.split('\n')]
 
     @property
     def image_name(self):
+        """
+            Returns the image name
+        """
         unique_str = ''.join(["'%s':'%s';"%(key, val) for (key, val) in sorted(self.info.items())]).encode('utf-8')
         return hashlib.sha1(unique_str).hexdigest()
 
@@ -85,6 +104,14 @@ class Framework:
         self.resource_man = ResourceManager(info['resource_manager'], self.base)        # Can't use this right now. 
     
     def docker_contents(self, image_name):
+        """
+            Upper level function to return the contents that need
+            to be in the Dockerfile
+
+            Returns
+            --------
+                docker_contents     -- list of all the commands.
+        """
         docker_contents = []
         # adding the base config (SSH and stuff). 
         docker_contents.extend(read_file(self.base + 'base_config'))
@@ -95,6 +122,13 @@ class Framework:
         return docker_contents
 
     def support(self):
+        """
+            Checks if we support the computation engine or not. 
+
+            Returns
+            --------
+                Boolean     -- If we can support or not. 
+        """
         if self.computation.support():          # self.computation.support() and self.resource_man.support()
             return True
         return False
@@ -110,12 +144,26 @@ class Engine:
         self.name = self.name = info['name']
 
     def support(self):
+        """
+            Checks if we support the computation engine or not. 
+
+            Returns
+            --------
+                Boolean     -- If we can support or not. 
+        """
         support_list = os.popen('ls ' + self.base).read()
         return self.name in [names.split(' ')[0] for names in support_list.split('\n')]
 
     def docker_contents(self):
+        """
+            Contents for the computation engine that need to be in the Dockerfile
+
+            Returns
+            --------
+                docker_contents     -- list of all the commands.
+        """
         docker_contents = []
-        docker_contents.extend(read_file(self.base + self.name + '/' + self.name + '_config'))
+        docker_contents.extend(read_file(self.config_path + '/' + self.name + '_config'))
         return docker_contents
     
     @property
@@ -133,10 +181,15 @@ class ComputationEngine(Engine):
         super().__init__(info, base)
         # TODO: Add more here @TANMAY
     
+    def update_config_files(self):
+        # TODO: @Tanmay -> this is where you can update the config files
+        pass
+
     def docker_contents(self, image_name):
         docker_contents = super().docker_contents()
         config_folder = self.base + self.name + '/config/.'
-        # print('cp -r ' + config_folder + ' ' + image_name + '/config')
+        # update the XML files
+        self.update_config_files()
         os.system('cp -r ' + config_folder + ' ' + image_name + '/config')
         return docker_contents
 
